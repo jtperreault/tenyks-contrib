f21e667ef0ff327e671a1e9be29763095b8df875
James T. Perreault <james@perreau.lt>
2013-10-15 16:06:22 -0400

    scaffold for gif retreival

---
+++ src/tenyksgifs/main.py
@@ -0,0 +1,12 @@
+import requests
+
+from tenyksclient.client import Client, run_client
+from tenyksclient.config import settings
+
+class TenyksGifs(Client):
+
+    def parse_tags():
+        # tag parsing
+
+    def fetch_gif():
+        # post tags
